d16cce9bb0b77a0d146e58bb9f0f7e9fea89812b
Le Nhat Minh <lenhatminhchodien@gmail.com>
2022-06-09 16:44:59 +0700

    09/06/2022 2

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -12,12 +12,11 @@ session_start();
 class CartController extends Controller
 {
     public function save_Cart(Request $request){
-        $cate_products = DB::table('tbl_category_products')->where('category_status','1')->orderby('category_id','desc')->get();
         $productID=$request->productid_hidden;
         $quantity=$request->qty;
-
         $data = DB::table('tbl_products')->where('product_id',$productID)->get();
-        return view('pages.cart.show_cart')->with('category', $cate_products);
+        $cate_products = DB::table('tbl_category_products')->where('category_status','1')->orderby('category_id','desc')->get();
+        return view('pages.cart.show_cart')->with('category',$cate_products);
 
     }
 }
--- resources/views/pages/cart/show_cart.blade.php
+++ resources/views/pages/cart/show_cart.blade.php
@@ -4,24 +4,23 @@
         <div class="container">
             <div class="breadcrumbs">
                 <ol class="breadcrumb">
-                    <li><a href="{{ URL::to('/') }}">Home</a></li>
-                    <li class="active">Shopping Cart</li>
+                    <li><a href="{{ URL::to('/') }}">Trang chủ</a></li>
+                    <li class="active">Giỏ hàng</li>
                 </ol>
             </div>
             <div class="table-responsive cart_info">
                 <table class="table table-condensed">
                     <thead>
                         <tr class="cart_menu">
-                            <td class="image">Item</td>
+                            <td class="image">Sản phẩm</td>
                             <td class="description"></td>
-                            <td class="price">Price</td>
-                            <td class="quantity">Quantity</td>
-                            <td class="total">Total</td>
+                            <td class="price">Giá</td>
+                            <td class="quantity">Số lượng</td>
+                            <td class="total">Tổng</td>
                             <td></td>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($category as $key => $cate)
                         <tr>
                             <td class="cart_product">
                                 <a href=""><img src="images/cart/one.png" alt=""></a>
@@ -48,11 +47,89 @@
                                 <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
                             </td>
                         </tr>
-                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </section>
     <!--/#cart_items-->
+
+    <section id="do_action">
+        <div class="container">
+            <div class="heading">
+                <h3>What would you like to do next?</h3>
+                <p>Choose if you have a discount code or reward points you want to use or would like to estimate your
+                    delivery cost.</p>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="chose_area">
+                        <ul class="user_option">
+                            <li>
+                                <input type="checkbox">
+                                <label>Use Coupon Code</label>
+                            </li>
+                            <li>
+                                <input type="checkbox">
+                                <label>Use Gift Voucher</label>
+                            </li>
+                            <li>
+                                <input type="checkbox">
+                                <label>Estimate Shipping & Taxes</label>
+                            </li>
+                        </ul>
+                        <ul class="user_info">
+                            <li class="single_field">
+                                <label>Country:</label>
+                                <select>
+                                    <option>United States</option>
+                                    <option>Bangladesh</option>
+                                    <option>UK</option>
+                                    <option>India</option>
+                                    <option>Pakistan</option>
+                                    <option>Ucrane</option>
+                                    <option>Canada</option>
+                                    <option>Dubai</option>
+                                </select>
+
+                            </li>
+                            <li class="single_field">
+                                <label>Region / State:</label>
+                                <select>
+                                    <option>Select</option>
+                                    <option>Dhaka</option>
+                                    <option>London</option>
+                                    <option>Dillih</option>
+                                    <option>Lahore</option>
+                                    <option>Alaska</option>
+                                    <option>Canada</option>
+                                    <option>Dubai</option>
+                                </select>
+
+                            </li>
+                            <li class="single_field zip-field">
+                                <label>Zip Code:</label>
+                                <input type="text">
+                            </li>
+                        </ul>
+                        <a class="btn btn-default update" href="">Get Quotes</a>
+                        <a class="btn btn-default check_out" href="">Continue</a>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="total_area">
+                        <ul>
+                            <li>Cart Sub Total <span>$59</span></li>
+                            <li>Eco Tax <span>$2</span></li>
+                            <li>Shipping Cost <span>Free</span></li>
+                            <li>Total <span>$61</span></li>
+                        </ul>
+                        <a class="btn btn-default update" href="">Update</a>
+                        <a class="btn btn-default check_out" href="">Check Out</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/#do_action-->
 @endsection
